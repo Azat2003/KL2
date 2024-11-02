aaaa46f54aba0dd601966678a6ae26a642d51b9f
Your Name <you@example.com>
2024-11-02 16:36:12 +0300

    In the second branch of the when statement, add a comma after the "Yellow" condition and then an "Amber" condition

---
+++ trafficLightColor when.kt
@@ -0,0 +1,10 @@
+fun main() {
+    val trafficLightColor = "Yellow"
+
+    when (trafficLightColor) {
+        "Red" -> println("Stop")
+        "Yellow", "Amber" -> println("Slow")
+        "Green" -> println("Go")
+        else -> println("Invalid traffic-light color")
+    }
+}
