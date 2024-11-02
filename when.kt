f5ab87719356132a289d509ca97b2398e9b3ff1b
Your Name <you@example.com>
2024-11-02 16:28:24 +0300

    add color when

--- when.kt
+++ when.kt
@@ -2,5 +2,8 @@ fun main() {
     val trafficLightColor = "Black"
 
     when (trafficLightColor) {
+        "Red" -> println("Stop")
+        "Yellow" -> println("Slow")
+        "Green" -> println("Go")
     }
 }
