0336938af811ce0bfd03289dac6eace48380a77e
Your Name <you@example.com>
2024-11-02 16:23:00 +0300

    trafficLightColor black

---
+++ trafficLightColor.kt
@@ -0,0 +1,11 @@
+fun main() {
+    val trafficLightColor = "Black"
+
+    if (trafficLightColor == "Red") {
+        println("Stop")
+    } else if (trafficLightColor == "Yellow") {
+        println("Slow")
+    } else {
+        println("Go")
+    }
+}
