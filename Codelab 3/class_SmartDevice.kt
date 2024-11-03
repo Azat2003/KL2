0ddbb8aa80476133c8d99ebb3a0c5a2f66238baf
Your Name <you@example.com>
2024-11-04 00:12:20 +0300

    add main

--- Codelab 3/class_SmartDevice.kt
+++ Codelab 3/class_SmartDevice.kt
@@ -1,6 +1,8 @@
 class SmartDevice {
 
     val name = "Android TV"
+    val category = "Entertainment"
+    var deviceStatus = "online"
 
     fun turnOn() {
         println("Smart device is turned on.")
@@ -9,4 +11,10 @@ class SmartDevice {
     fun turnOff() {
         println("Smart device is turned off.")
     }
+}
+fun main() {
+    val smartTvDevice = SmartDevice()
+    println("Device name is: ${smartTvDevice.name}")
+    smartTvDevice.turnOn()
+    smartTvDevice.turnOff()
 }
