2fdcb033acbde9c386e9837320ce18c807ba4e8d
Your Name <you@example.com>
2024-11-04 00:06:53 +0300

    class SmartDevice

---
+++ Codelab 3/class_SmartDevice.kt
@@ -0,0 +1,12 @@
+class SmartDevice {
+
+    val name = "Android TV"
+
+    fun turnOn() {
+        println("Smart device is turned on.")
+    }
+
+    fun turnOff() {
+        println("Smart device is turned off.")
+    }
+}
