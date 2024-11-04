a441f2ec439bafc596ae16232bb8409b33ce72d7
User <you@example.com>
2024-11-04 10:53:11 +0300

    speakerVolume

---
+++ Codelab 3/speakerVolume.kt
@@ -0,0 +1,10 @@
+class SmartTvDevice(deviceName: String, deviceCategory: String) :
+    SmartDevice(name = deviceName, category = deviceCategory) {
+
+    var speakerVolume = 2
+        set(value) {
+            if (value in 0..100) {
+                field = value
+            }
+        }
+}
