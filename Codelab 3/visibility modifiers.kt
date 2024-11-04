a0f478ce263fd16b459ca40379095f85328445f0
User <you@example.com>
2024-11-05 02:38:32 +0300

    private

--- Codelab 3/visibility modifiers.kt
+++ Codelab 3/visibility modifiers.kt
@@ -1,11 +1,33 @@
-open class SmartDevice(val name: String, val category: String) {
+class SmartHome(
+    val smartTvDevice: SmartTvDevice,
+    val smartLightDevice: SmartLightDevice
+) {
+
+    var deviceTurnOnCount = 0
+        private set
+
+    fun turnOnTv() {
+        deviceTurnOnCount++
+        smartTvDevice.turnOn()
+    }
+
+    fun turnOffTv() {
+        deviceTurnOnCount--
+        smartTvDevice.turnOff()
+    }
+    
+    ...
+
+    fun turnOnLight() {
+        deviceTurnOnCount++
+        smartLightDevice.turnOn()
+    }
+
+    fun turnOffLight() {
+        deviceTurnOnCount--
+        smartLightDevice.turnOff()
+    }
 
     ...
 
-    var deviceStatus = "online"
-        protected set(value) {
-           field = value
-       }
-
-    ...
 }
