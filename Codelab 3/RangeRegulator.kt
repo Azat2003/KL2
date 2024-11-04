60cbe5880c44a3eafb7af6c64268f4ed512fad8c
User <you@example.com>
2024-11-05 02:41:22 +0300

    add return

--- Codelab 3/RangeRegulator.kt
+++ Codelab 3/RangeRegulator.kt
@@ -4,9 +4,15 @@ class RangeRegulator(
     private val maxValue: Int
 ) : ReadWriteProperty<Any?, Int> {
 
+    var fieldData = initialValue
+
     override fun getValue(thisRef: Any?, property: KProperty<*>): Int {
+        return fieldData
     }
 
     override fun setValue(thisRef: Any?, property: KProperty<*>, value: Int) {
+        if (value in minValue..maxValue) {
+            fieldData = value
+        }
     }
 }
