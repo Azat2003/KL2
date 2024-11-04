8744125c3c90fc71fb20115075fbcb9d753a3cd7
User <you@example.com>
2024-11-04 11:14:53 +0300

    SmartLightDevice

---
+++ Codelab 3/SmartLightDevice.kt
@@ -0,0 +1,21 @@
+class SmartLightDevice(deviceName: String, deviceCategory: String) :
+    SmartDevice(name = deviceName, category = deviceCategory) {
+
+    var brightnessLevel = 0
+        set(value) {
+            if (value in 0..100) {
+                field = value
+            }
+        }
+
+    fun increaseBrightness() {
+        brightnessLevel++
+        println("Brightness increased to $brightnessLevel.")
+    }
+
+    fun turnOn() {
+        deviceStatus = "on"
+        brightnessLevel = 2
+        println("$name turned on. The brightness level is $brightnessLevel.")
+    }
+}
