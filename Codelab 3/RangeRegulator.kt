5260e28f55b24f7040709cb1d81661db2de7bbcb
User <you@example.com>
2024-11-05 02:40:08 +0300

    add override

--- Codelab 3/RangeRegulator.kt
+++ Codelab 3/RangeRegulator.kt
@@ -4,4 +4,9 @@ class RangeRegulator(
     private val maxValue: Int
 ) : ReadWriteProperty<Any?, Int> {
 
+    override fun getValue(thisRef: Any?, property: KProperty<*>): Int {
+    }
+
+    override fun setValue(thisRef: Any?, property: KProperty<*>, value: Int) {
+    }
 }
