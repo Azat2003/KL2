f18edb259df2603d0cd2d6ffa239fcef7b48fb30
User <you@example.com>
2024-11-04 11:25:35 +0300

    add the override keyword

--- Codelab 3/SmartLightDevice.kt
+++ Codelab 3/SmartLightDevice.kt
@@ -1,27 +1,40 @@
-class SmartLightDevice(deviceName: String, deviceCategory: String) :
+class SmartTvDevice(deviceName: String, deviceCategory: String) :
     SmartDevice(name = deviceName, category = deviceCategory) {
 
-    var brightnessLevel = 0
+    var speakerVolume = 2
         set(value) {
             if (value in 0..100) {
                 field = value
             }
         }
 
-    fun increaseBrightness() {
-        brightnessLevel++
-        println("Brightness increased to $brightnessLevel.")
+    var channelNumber = 1
+        set(value) {
+            if (value in 0..200) {
+                field = value
+            }
+        }
+
+    fun increaseSpeakerVolume() {
+        speakerVolume++
+        println("Speaker volume increased to $speakerVolume.")
+    }
+
+    fun nextChannel() {
+        channelNumber++
+        println("Channel number increased to $channelNumber.")
     }
 
     override fun turnOn() {
         deviceStatus = "on"
-        brightnessLevel = 2
-        println("$name turned on. The brightness level is $brightnessLevel.")
+        println(
+            "$name is turned on. Speaker volume is set to $speakerVolume and channel number is " +
+                "set to $channelNumber."
+        )
     }
 
     override fun turnOff() {
         deviceStatus = "off"
-        brightnessLevel = 0
-        println("Smart Light turned off")
+        println("$name turned off")
     }
 }
