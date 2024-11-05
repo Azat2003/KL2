f5a1bf2d123766da1d4e71e1f387e7c32abaa43b
User <you@example.com>
2024-11-05 03:35:36 +0300

    safe call operator ?.

--- Codelab 5/Special auction.kt
+++ Codelab 5/Special auction.kt
@@ -8,5 +8,5 @@ fun main() {
 class Bid(val amount: Int, val bidder: String)
  
 fun auctionPrice(bid: Bid?, minimumPrice: Int): Int {
-   // Fill in the code.
+    return bid?.amount ?: minimumPrice
 }
