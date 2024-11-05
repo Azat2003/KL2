0d388ae02511a98faf8cb341303c6f6ecd8c0a2a
User <you@example.com>
2024-11-05 03:12:35 +0300

    add val isPopular

--- Codelab 5/Song.kt
+++ Codelab 5/Song.kt
@@ -10,4 +10,11 @@ class Song(
     val artist: String, 
     val yearPublished: Int, 
     val playCount: Int
-)
+){
+    val isPopular: Boolean
+        get() = playCount >= 1000
+
+    fun printDescription() {
+        println("$title, performed by $artist, was released in $yearPublished.")
+    }   
+}
