4a5ce076e2b0021b438b3aaf6bf2ab3f9a5d797a
User <you@example.com>
2024-11-05 03:05:30 +0300

    return when

--- Codelab 5/ticketPrice.kt
+++ Codelab 5/ticketPrice.kt
@@ -11,5 +11,10 @@ fun main() {
 }
 
 fun ticketPrice(age: Int, isMonday: Boolean): Int {
-    // Fill in the code.
+    return when(age) {
+        in 0..12 -> 15
+        in 13..60 -> if (isMonday) 25 else 30
+        in 61..100 -> 20
+        else -> -1
+    }
 }
